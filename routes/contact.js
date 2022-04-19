e8c74612e34b838d81e1018ab81f5da622b42f85
prasharma0 <prashant.sharma.ps725@gmail.com>
2022-04-19 12:56:57 +0545

    handled the edit request endpoint

--- routes/contact.js
+++ routes/contact.js
@@ -33,9 +33,29 @@ router.get("/mycontacts", auth, async (req, res)=>{
        console.log(err);
     }
 });
+
+//update or edit a contact
+router.put("/contact", auth ,  async(req, res)=>{
+    const{id} = req.body;
+    if(!id)return res.status(400).json({error:"no id specified."})
+    if(!mongoose.isValidObjectId(id)) return res.status(400).json({error: "please enter the valid id."})
+    try {
+        const contact = await Contact.findOne({_id :id});
+
+        if (req.user._id.toString() !== contact.postedBy._id.toString())
+        return res.status(401).json({error:"you can't edit other's contacts!"});
+        const updatedData = {...req.body, id: undefined};
+
+        const result = await Contact.updateOne({_id:id}, updatedData);
+        return res.status(200).json({...contact._doc});
+
+    } catch (err) {
+        console.log(err);
+    }
+})
 //delete a contact
 router.delete("/delete/:id", auth, async (req, res)=>{
-    const {id} = req.params;
+    const {id} = req.params; 
     if(!id)return res.status(400).json({error:"no id specified."})
     if(!mongoose.isValidObjectId(id)) return res.status(400).json({error: "please enter the valid id."})
     try {
